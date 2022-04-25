4b097bca44d71aee6fc901872cc5e4f4f50e7495
Leonore Yardimli <leonore1982@gmail.com>
2022-04-25 16:51:44 +0800

    Initial Commit

---
+++ EgeClassPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EgeClassPlayground
+//
+//  Created by Leonore Yardimli on 2022/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EgeClassPlayground/EgeClassPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  EgeClassPlaygroundApp.swift
+//  EgeClassPlayground
+//
+//  Created by Leonore Yardimli on 2022/4/25.
+//
+
+import SwiftUI
+
+@main
+struct EgeClassPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
